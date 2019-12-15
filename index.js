8cf56451348b1689ad8a1f87455653b8b238294b
Nick <golub.nick@gmail.com>
2019-12-15 19:07:40 +0200

    added changes

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ let TelegramBot = require('node-telegram-bot-api');
 
 let token = '973150418:AAFpWeO1cQtNRQCe1IivhsBPyxEOfyHiTik';
 let bot = new TelegramBot(token, {polling: true});
-let users  = [{name: 'Alex', id : '55033367', day: ''}];
+// let users  = [{name: 'Alex', id : '55033367', day: ''}];
+let users  = [];
+let flag = false;
 
 const arrWeek = [
     {name: 'monday', place_time: 'Магазин в 9:35'},
@@ -33,40 +35,63 @@ bot.onText(/отписка/, function (msg) {
     bot.sendMessage(userId, 'Вы отписались от рассылку!');
 });
 
-bot.onText(/week/, function (msg, match) {
+bot.on('message', function (msg) {
     let userId = msg.from.id;
-    let options = {
-        reply_markup: JSON.stringify({
-            inline_keyboard: [
-                [{text: 'Понедельник', callback_data: arrWeek[0].name}],
-                [{text: 'Вторник', callback_data: arrWeek[1].name}],
-                [{text: 'Среда', callback_data: arrWeek[2].name}],
-                [{text: 'Четверг', callback_data: arrWeek[3].name}],
-                [{text: 'Пятница', callback_data: arrWeek[4].name}]
-            ]
-        })
-    };
-    console.log(`userId is ${userId}`);
-    bot.sendMessage(userId, 'Выберите день недели:', options);
+    if (msg.text === '/week'){
+        let user = users.filter(w => w.id === userId)[0];
+        if (user){
+            bot.sendMessage(userId, `Добрый день`);
+            msg.text = user.name;
+            selectDay(msg, true);
+        } else {
+            flag = true;
+            bot.sendMessage(userId, 'Добрый день назовите ваше имя пожалуйста');
+        }
+    } else {
+        selectDay(msg, flag);
+    }
 });
 
 bot.on('callback_query', function (msg) {
-    let day;
     let answer = msg.data;
     let userId = msg.from.id;
-    console.log(answer);
     arrWeek.forEach(x => {
         if ((x.name).includes(answer)) {
-            day = x.name;
-            console.log(day);
+            let user = users.filter(w => w.id === userId)[0];
+            user.day = x.name;
             bot.sendMessage(userId, x.place_time);
             bot.sendMessage(userId, 'Подтвердите выше присутствие', arrButtons);
         }
     });
     if (answer === 'positive') {
+        let user = users.filter(w => w.id === userId)[0];
         bot.sendMessage(userId, 'Спасибо, ожидаем вас.');
-        bot.sendMessage('1019762042', `${userId} будет в ${day}`);
+        bot.sendMessage('1019762042', `${user.name} будет в ${user.day}`);
     } else if (answer === 'negative') {
         bot.sendMessage(userId, 'Спасибо, что воспользовались ботом.');
     }
 });
+
+function selectDay(msg, isWeek) {
+    if (isWeek) {
+        let userName = msg.text;
+        let userId = msg.from.id;
+        let options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{text: 'Понедельник', callback_data: arrWeek[0].name}],
+                    [{text: 'Вторник', callback_data: arrWeek[1].name}],
+                    [{text: 'Среда', callback_data: arrWeek[2].name}],
+                    [{text: 'Четверг', callback_data: arrWeek[3].name}],
+                    [{text: 'Пятница', callback_data: arrWeek[4].name}]
+                ]
+            })
+        };
+        let user = users.filter(w => w.id === userId)[0];
+        if (!user) {
+            users.push({name: userName, id: userId})
+        }
+        flag = false;
+        bot.sendMessage(userId, `${userName} выберите день недели:`, options);
+    }
+}
